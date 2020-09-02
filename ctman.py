238fbccdb16ae65ec72100b935e2ee09b9676762
blesswood <40756812+blesswood@users.noreply.github.com>
2020-09-02 22:54:22 +0300

    Add files via upload

---
+++ ctman.py
@@ -0,0 +1,37 @@
+import os
+import sys
+
+askme = input("1)Build manpage\n2)Create manpage\n")
+
+def build_from_file():
+    if(os.getuid()) == 0: #check privileges
+        filename = input("->Manpage: ")
+        pwd = str(os.popen('pwd').read().split())[2:-2] #current pwd
+        r = "cp " +  filename + " /usr/share/man/man1/" + filename + ".1" #copy manpage to /man
+        mute = os.popen(r)
+        r = 'gzip /usr/share/man/man1/' + filename + '.1' #zip manpage
+        mute = os.popen(r)
+        print("\n--->Try: man %s\n" % filename)
+    else:
+        print("\nTry with sudo!")
+        sys.exit()
+
+def create_man(filename):
+    os.popen("touch  %s" % filename) #create manpage
+    manpage = open(filename, "w")
+
+    text = '''.\\" "TITLE"
+.TH man 1 "DATE" "VERSION"
+'''
+    for opt in open("options.txt", "r"):
+        text += "\n.SH " + opt #add to manpage additional options from options.txt
+    manpage.write(text)
+    print("\n--->Edit '%s' in current directory and start ctman.py to build manpage\n" % filename)
+
+if "1" in askme:
+    build_from_file() #build manpage
+elif "2" in askme:
+    filename = input("->Manpage name: ")
+    create_man(filename) #create manpage
+else:
+    print("--->Sorry, incorrect data")
